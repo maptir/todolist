26a8a3b685ca9eb2a89fedcf3d388b5280f3286e
Archawin Tirugsapun <zepalzth@gmail.com>
2017-08-30 19:27:29 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,59 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './containers/Header';
+import Content from './containers/Content';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: '',
+      todos: JSON.parse(localStorage.getItem('todos')) || []
+    }
+  }
+
+  inputText = (input) => {
+    this.setState({
+      text: input.target.value
+    })
+  }
+
+  addTodo = () => {
+    if (this.state.text === "") return;
+    const todo = {
+      text: this.state.text
+    }
+    const todos = [...this.state.todos, todo];
+    this.setState({
+      todos: todos,
+      text: ''
+    })
+    console.log(JSON.parse(localStorage.getItem('todos')));
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }
+
+  removeTodo = (index) => () => {
+    const todos = [...this.state.todos.slice(0, index), ...this.state.todos.slice(index + 1)];
+    this.setState({
+      todos: todos
+    })
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }
+
   render() {
     return (
       <div className="App">
         <Header />
+        <Content inputText={this.inputText} text={this.state.text} addTodo={this.addTodo} />
+        {
+          this.state.todos.map((todo, index) => (
+            <div className="todo-wrapper">
+              <div className="todo">{todo.text}</div>
+              <input className="input-checkout" type="checkbox" />
+              <button className="remove-btn" onClick={this.removeTodo(index)}> x </button>
+            </div>
+          ))
+        }
       </div>
     );
   }
---
+++ src/containers/Content.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Content extends Component {
+    render() {
+        return (
+            <div className="content">
+                <input className="todo-input" type="text" onChange={this.props.inputText} value={this.props.text} />
+                <button className="todo-btn" onClick={this.props.addTodo}> + </button>
+            </div>
+        );
+    }
+}
+
+export default Content;
